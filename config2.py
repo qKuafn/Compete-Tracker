325a969cc9cd2c8c190e40fcb9738e7c6fa549da
qKuafn <qkuafn@gmail.com>
2025-07-29 22:50:57 +0900

    36.30

--- config2.py
+++ config2.py
@@ -32,5 +32,5 @@ if test is True:
     Tournament_Webhook = False
     Hotfix_Webhook = False
 
-version = "++Fortnite+Release-36.20"
-build = "44118662"
+version = "++Fortnite+Release-36.30"
+build = "44367537"
